93355f1c4a605a312300cdc45ae952bbc1967f4f
rude4444 <rudolfadmiration@gmail.com>
2023-04-22 21:37:32 +0900

    cw task1.js add

--- cw_task1.js
+++ cw_task1.js
@@ -1,1 +1,14 @@
-const a = 3;
+function getCount(str) {
+    const vowels = ["a", "e", "i", "o", "u"]
+  
+    let total = 0;
+
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            total++;
+        }
+    }
+
+    // return number of vowels
+    return total;
+}
